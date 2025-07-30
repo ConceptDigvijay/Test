0701332d0536627fb3ae5cd49bef19e9a6613852
Digvijay Wagh <iamdigvijaywagh@gmail.com>
2025-07-30 20:12:26 +0530

    Initialize Laravel project with admin panel and localization

---
+++ laravel_app/lang/mr/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'ही माहिती आमच्या रेकॉर्डशी जुळत नाही.',
+    'password' => 'प्रदान केलेला पासवर्ड चुकीचा आहे.',
+    'throttle' => 'खूप वेळा लॉगिन करण्याचा प्रयत्न झाला. :seconds सेकंदांनी पुन्हा प्रयत्न करा.',
+
+];
---
+++ laravel_app/resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div class="py-12 text-center">
+        <h1 class="text-3xl font-bold mb-4">{{ __('messages.welcome') }}</h1>
+    </div>
+</x-app-layout>
